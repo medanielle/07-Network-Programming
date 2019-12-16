fae84e36ffed79ec4ba6c5dee885bcb4fd439f43
Danielle Hughett <medanielle@gmail.com>
2019-12-16 11:29:47 -0600

    cc

---
+++ Notes/socket.py
@@ -0,0 +1,10 @@
+import socket
+
+print(socket.gethostname)
+
+print(socket.gethostbyname("google.com"))
+
+# s.bind(("", 10000)) +> specific port "" = local host
+
+
+# https://github.com/CyberTrainingUSAF/07-Network-Programming/tree/master/09_NetworkingExtended/SocketProgramming
